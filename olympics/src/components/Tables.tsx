e7ca2390144b3de0472524526489b0fab6b52798
Rayan Muryell <rayan_muryell@hotmail.com>
2024-07-31 15:37:27 -0300

    fix: no-cors api

--- olympics/src/components/Tables.tsx
+++ olympics/src/components/Tables.tsx
@@ -27,7 +27,7 @@ const Tables: React.FC = () => {
       setLoading(true);
       try {
         const response = await fetch("https://api.olympics.kevle.xyz/medals", {
-          mode: "cors",
+          mode: "no-cors",
         });
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
